b5e34721449d26d28b09849fc92dec15eaf25e9f
thanghiii <tatthang0981@gmail.com>
2024-08-11 23:21:20 +0700

    cập nhât code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// const ASSET_URL = process.env.ASSET_URL || '';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/DoanKy/",
+});
